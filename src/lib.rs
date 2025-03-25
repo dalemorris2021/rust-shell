bb62ad773b6ebf42ac49c02a122f30c16ad5a5ad
Dale Morris <dalemorris2021@gmail.com>
2025-03-25 01:42:13 -0400

    Moved arg parsing logic from main to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,24 @@
 use std::{error::Error, fmt::Debug, path::PathBuf};
 
+use clap::Parser;
+
+/// A shell for the Flinstone Disk Project
+#[derive(Debug, Parser)]
+#[command(version, about, long_about = None)]
+pub struct Args {
+    /// Input file
+    #[arg(short, default_value_t = String::from(""))]
+    pub input_file: String,
+
+    /// Print files
+    #[arg(long)]
+    pub dir: bool,
+
+    /// Print contents of file
+    #[arg(long, default_value_t = String::from(""))]
+    pub type_: String,
+}
+
 #[derive(Debug)]
 pub enum ShellAction {
     Disk,
@@ -14,17 +33,17 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(input_file: &str, dir: bool, type_: &str) -> Self {
-        let input_path = if input_file == "" {
+    pub fn build(args: Args) -> Self {
+        let input_path = if args.input_file == "" {
             None
         } else {
-            Some(PathBuf::from(input_file))
+            Some(PathBuf::from(args.input_file))
         };
 
-        let action = if dir {
+        let action = if args.dir {
             ShellAction::Dir
-        } else if type_ != "" {
-            ShellAction::Type(String::from(type_))
+        } else if args.type_ != "" {
+            ShellAction::Type(String::from(args.type_))
         } else {
             ShellAction::Disk
         };
--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,9 @@ use std::process;
 use clap::Parser;
 use shell::Config;
 
-/// A shell for the Flinstone Disk Project
-#[derive(Debug, Parser)]
-#[command(version, about, long_about = None)]
-struct Args {
-    /// Input file
-    #[arg(short, default_value_t = String::from(""))]
-    input_file: String,
-
-    /// Print files
-    #[arg(long)]
-    dir: bool,
-
-    /// Print contents of file
-    #[arg(long, default_value_t = String::from(""))]
-    type_: String,
-}
-
 fn main() {
-    let args = Args::parse();
-    let config = Config::build(&args.input_file, args.dir, &args.type_);
+    let args = shell::Args::parse();
+    let config = Config::build(args);
 
     if let Err(e) = shell::run(config) {
         eprintln!("Application error: {e}");
